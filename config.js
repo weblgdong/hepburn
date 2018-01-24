a606f222f7278906d4c43a31d408269003dcd285
weblgdong <weblgdong@gmail.com>
2018-01-24 23:12:21 +0800

    update

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-const wechat ={
-  'hostname':'',
-  'openid':'',
-  'secret':''
+const wechat = {
+    'hostname': 'http://lgdong.cn/',
+    'openid': 'wx077ee794dee2ba70',
+    'secret': '1f249ba470a6b8884797556cfa2bdf34'
 }
 
 module.exports = {
-  wechat
+    wechat
 }
