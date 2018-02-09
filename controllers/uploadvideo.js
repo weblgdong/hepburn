dd53f40c2a4910e9fe2e29b927fd6c455b37866b
weblgdong <weblgdong@gmail.com>
2018-02-09 21:38:43 +0800

    update

--- controllers/uploadvideo.js
+++ controllers/uploadvideo.js
@@ -1,13 +1,13 @@
 const getUerInfo = require('../util/getUerInfo');
 const findUserBlessing = require('../controllers/findUserBlessing');
 const updateUserInfo = require('../controllers/updateUserInfo');
-const createCaller = require('./caller');
+// const createCaller = require('./caller');
 
 module.exports = async(ctx) => {
     let { access_token, openid } = ctx.session;
     let data = await getUerInfo(access_token, openid);
     let res = await findUserBlessing(openid);
-    await createCaller(data);
+    // await createCaller(data);
     
     data = JSON.parse(data);
     let { nickname, headimgurl } = data;
